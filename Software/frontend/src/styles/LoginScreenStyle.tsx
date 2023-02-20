58f894167868b6f86cb878d71edfc95c3636bbcf
Alex Souphanthong <alexsouphanthong@macbook-pro-2.lan>
2023-02-19 22:26:52 -0500

    Initalized backend and frontend for mobile app

---
+++ Software/frontend/src/styles/LoginScreenStyle.tsx
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+
+export const LoginStyle = StyleSheet.create({
+    loginBox:{
+        height:75,
+        width:200
+    },
+
+    container:{
+        marginTop:100,  
+        alignItems:'center',
+        justifyContent:'center'
+    },
+
+    containter2:{
+        justifyContent:'space-evenly',
+        height:500
+    }
+    
+
+
+}
+
+
+);
