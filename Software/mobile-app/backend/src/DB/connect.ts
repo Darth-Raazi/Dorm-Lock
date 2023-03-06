121727a5012996a1c026b75421b7a36e2adc8111
Alex Souphanthong <alexsouphanthong@macbook-pro-2.lan>
2023-03-05 22:49:59 -0500

    created backend authentication for user

---
+++ Software/mobile-app/backend/src/DB/connect.ts
@@ -0,0 +1,26 @@
+import { connect, Model, Document} from 'mongoose';
+import * as dotenv from 'dotenv';
+import { UserModel } from '../models/users/userModel';
+import { IUserDocument } from '../models/users/userType';
+
+dotenv.config();
+
+export const models: {users?: Model<IUserDocument>
+                     teams?: Model<Document> 
+                    resources?: Model<Document> } =
+                     {  users: UserModel
+
+                    };
+
+                        
+
+export async function connectToDB(){
+    try{
+        const database = await connect(process.env.DB_CONN_STRING as string);
+        console.log("Succesfully connect to DB")
+
+    }catch(error){
+        console.error(error);
+    }
+}
+
---
+++ Software/mobile-app/backend/src/models/users/userType.ts
@@ -0,0 +1,10 @@
+import { Document, Model } from 'mongoose';
+
+export interface IUser{
+    username:string,
+    passwordHash:string
+
+}
+
+export interface IUserDocument extends IUser, Document{};
+export interface IUserModel extends Model<IUserDocument>{};
